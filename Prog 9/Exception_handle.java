a8da10c6b8a6ca78f153179f400accc35fd3e52c
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-06-19 07:15:23 +0530

    Exception_handle done

---
+++ Prog 9/Exception_handle.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+class Exception_handle {
+    public static void main(String[] args) {
+        int a = 10, b = 5, c = 5, x, y;
+
+        try {
+            x = a / (b - c);
+        } catch (ArithmeticException e) {
+            System.out.println("DIVISION BY ZERO");
+        }
+        y = a / (b + c);
+        System.out.println("y=" + y);
+    }
+}
